47a061dc89faff88a16aca170f6830e32de39762
Marchay <40936964+Marchay@users.noreply.github.com>
2018-11-17 23:25:39 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,4 @@
 <?php
-return array(
-        'api' => 'http://149.28.102.229:13002',
-        'blockTargetInterval' => 30,
-        'coinUnits' => 100
-);
+$coinunit = '100'; //Coin units
 $daemonip = '127.0.0.1'; //Daemon IP
 $daemonport = '13002'; //Daemon Port
