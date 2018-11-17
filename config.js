81befa98735228ba6de98f79411cf9e7517482ff
Marchay <40936964+Marchay@users.noreply.github.com>
2018-11-17 23:08:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,8 @@
-var api = 'http://149.28.102.229:13002';
+var api = 'http://websiteip:13002'; //change websiteip to the website ip lol and : `port number` which defaults to 13002 
 var donationAddress = "";
 var blockTargetInterval = 40;
 var coinUnits = 100;
 var symbol = 'mtip';
 var refreshDelay = 30000;
 // pools stats by MainCoins
-var networkStat = {
- "mtip": [
-	["mtip.wahaobi.com", "http://mtip.wahaobi.com/:8121"]
- ]
-};
 
-var networkStat2 = {
-    "mtip": [
-	[""]
- ]
-};
